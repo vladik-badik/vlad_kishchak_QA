91bb775aaba62be08efa912ecb1001f766bbffb7
VladKishchak <vladyslav.k@volpis.com>
2023-04-02 23:32:45 +0300

    Task2 + Task3

---
+++ Homework2.py
@@ -0,0 +1,8 @@
+
+a = ['United States', 'France', 'Japan']
+b = ['Washington', 'Paris', 'Tokio']
+
+print (a[0], b[0], sep=': ')
+print (a[1], b[1], sep=': ')
+print (a[2], b[2], sep=': ')
+
---
+++ Homework3.py
@@ -0,0 +1,5 @@
+a = input ("Enter first integer value:")
+b = input ("Enter second integer value:")
+print (int(a) + int(b))
+print (int(a) * int(b))
+
