60e7d4446c6726be59ba08f5148d5182e415c847
vladik-badik <79592890+vladik-badik@users.noreply.github.com>
2023-04-02 10:50:36 +0300

    homewokr1

---
+++ Homework 1.py
@@ -0,0 +1,10 @@
+#print ("Jinger", "Damir", "Luna" , sep=', ')
+
+#print ("Greta", "Vincent", "Sharik" , sep=' ')
+
+
+a = "Jinger"
+b = "Damir"
+c = "Luna"
+print (a, b, c, sep=', ')
+print (a, b, c, sep=' ')
