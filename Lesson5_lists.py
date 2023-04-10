805e752cc738787454d74d5e7ae44455171e1528
VladKishchak <vladyslav.k@volpis.com>
2023-04-10 20:17:19 +0300

    update

---
+++ Lesson5_lists.py
@@ -0,0 +1,29 @@
+#### min, max, sum,sorted
+# a = [1,33,5.7]
+# print(sum(a))
+
+# a=[1,33,10]
+# print(max(a))
+# print(min(a))
+
+
+# lst = [ 3,2,7,1,5]
+# lst2 = sorted(lst)
+# lst.sort()
+# # print(res)
+# print(lst)
+
+#########
+
+lst =[3,2,7,1,5,4,5,6]
+
+
+# lst.append(333) ### метод апенд додає в список елемент
+# print (lst)
+
+# print(lst.count(5))
+
+lst2 = [1,2,3]
+lst.extend(lst2)
+print(lst)
+print(lst2)
