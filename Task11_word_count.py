f1e2fd40b4e12c27b9dfb1c2b8e249cecec83d06
VladKishchak <vladyslav.k@volpis.com>
2023-04-15 19:09:53 +0300

    Task11_update

--- Task11_word_count.py
+++ Task11_word_count.py
@@ -1,9 +1,10 @@
-# рахуємо слова в реченнях яке вводить користувач
-user_sentence = input("Enter text:")
-sentences = user_sentence.split(".")
-# або ми можем рахувати вже слова в дефолтному реченні
-# multi_string = "Hello my name is Giovany Georgio. But everybody calls me Georgio."
-# sentences = multi_string.split(".")
+##### рахуємо слова в реченнях яке вводить користувач
+# user_sentence = input("Enter text:")
+# sentences = user_sentence.split(".")
+#### або ми можем рахувати вже слова в дефолтному реченні
+multi_string = "Hello my name is Giovany Georgio. But everybody calls me Georgio."
+sentences = multi_string.split(".")
+print("multi_string =" , multi_string)
 word_counts = []
 for sentence in sentences:
     words = sentence.strip().split(" ")
@@ -11,4 +12,4 @@ for sentence in sentences:
         word_count = len([word for word in words if word])
         word_counts.append(word_count)
 
-print(word_counts)
+print("words_number -> ",word_counts)
