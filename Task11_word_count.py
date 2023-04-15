0c4466d7d7fe8a8c96ddf7eb6b1dd64cd72de3f2
VladKishchak <vladyslav.k@volpis.com>
2023-04-15 21:20:50 +0300

    Upadte

--- Task11_word_count.py
+++ Task11_word_count.py
@@ -2,13 +2,13 @@
 # user_sentence = input("Enter text:")
 # sentences = user_sentence.split(".")
 #### або ми можем рахувати вже слова в дефолтному реченні
-multi_string = "Hello my name is Giovany Georgio. But everybody calls me Georgio."
+multi_string = "Hello my name is Giovany Georgio. But everybody calls me Georgio. Yes"
 sentences = multi_string.split(".")
 print("multi_string =" , multi_string)
 word_counts = []
 for sentence in sentences:
     words = sentence.strip().split(" ")
-    if len(words) > 1:
+    if len(words) >= 1:
         word_count = len([word for word in words if word])
         word_counts.append(word_count)
 
