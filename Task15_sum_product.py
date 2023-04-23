38b5dfead0fd8210ac94a16c31f9759f8ec37828
VladKishchak <vladyslav.k@volpis.com>
2023-04-23 12:36:55 +0300

    update

--- Task15_sum_product.py
+++ Task15_sum_product.py
@@ -1,9 +1,3 @@
-# my_list = [2, 4, 6, 2, 1, 1, 9, 4, 6]
-# my_min = 3
-# my_max = 6
-# my_sum, my_prod = sum_and_product(my_list, my_min, my_max)
-# print(f"The sum of elements between {my_min} and {my_max} is {my_sum}")
-# print(f"The product of elements between {my_min} and {my_max} is {my_prod}")
 
 my_list = [2, 4, 6, 2, 1, 1, 9, 4, 6]
 my_min = 2
