fcef386fd1562fe3a541d90247318002c1f7a033
VladKishchak <vladyslav.k@volpis.com>
2023-04-27 21:55:44 +0300

    task17 unfinished

---
+++ task17_sortingLst.py
@@ -0,0 +1,11 @@
+lst = [['a', 'c', 'd'], ['f', 'b', 'a'], ['a', 'n', 'k'], ['e', 'l', 'i']]
+
+for col in range(len(lst[0])):
+    column = []
+    for row in range(len(lst)):
+        column.append(lst[row][col])
+    column.sort()
+    for row in range(len(lst)):
+        lst[row][col] = column[row]
+
+print(lst)
