08191e7a8f9c45d015e0cd8d4b726ba731326ae5
VladKishchak <vladyslav.k@volpis.com>
2023-04-27 20:52:12 +0300

    Task24

---
+++ task24_secondLargest_number.py
@@ -0,0 +1,15 @@
+lst=[1,2,3,4,5,7,10,22,45,45.5]
+def second_largest(lst):
+    if len(lst) < 2:
+        return None
+
+    largest = second_largest = float('-inf')
+    for num in lst:
+        if num > largest:
+            second_largest = largest
+            largest = num
+        elif num > second_largest and num != largest:
+            second_largest = num
+
+    return second_largest if second_largest != float('-inf') else Non
+print(second_largest(lst))
