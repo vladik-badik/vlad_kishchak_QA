ff34079a53ce8cfe39dbf7865ad2dac7636b3221
VladKishchak <vladyslav.k@volpis.com>
2023-04-27 13:19:14 +0300

    update

--- lesson8_Function (cont.). Decorators. Generators.py
+++ lesson8_Function (cont.). Decorators. Generators.py
@@ -1,1 +1,12 @@
-print ("1")
+#####Iterators
+# lst =[1,2,3]
+# for i in lst:
+#     print(i)
+
+lst = [1, 2, 3]
+lst_iter = iter(lst)
+# print(type(lst_iter))
+print(next(lst_iter))
+print(next(lst_iter))
+print(next(lst_iter))
+# print(next(lst_iter
