85ed541e9b6b5083830b37210577643e3b04fa6f
VladKishchak <vladyslav.k@volpis.com>
2023-04-28 11:16:47 +0300

    update

--- Task22_lastSymbols.py
+++ Task22_lastSymbols.py
@@ -1,7 +1,7 @@
 def read_last(file_path, symbol_number):
-    with open('strings.txt', 'r') as f:
+    with open('read_last.txt', 'r') as f:
         for line in f:
             line = line.rstrip('\n')
             if line:
                 print(line[-symbol_number:])
-read_last('string.txt', 1)
+read_last('read_last.txt', 1)
