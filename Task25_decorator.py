a480f49a283c09b0138b9e2b67f01e880b81e9b2
VladKishchak <vladyslav.k@volpis.com>
2023-04-28 18:34:03 +0300

    Task25

---
+++ Task25_decorator.py
@@ -0,0 +1,16 @@
+def call_counter(filename):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            wrapper.count += 1
+            with open(filename, 'w') as f:
+                f.write(f"Function {func.__name__} was called {wrapper.count} times")
+            return func(*args, **kwargs)
+        wrapper.count = 0
+        return wrapper
+    return decorator
+@call_counter('log.txt')
+def add():
+    print("hello world")
+
+add()
+add()
